173c509f017a714230a301603918323708eafca6
devchea <rchea817@gmail.com>
2021-02-18 09:14:01 -0800

    "updates"

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,15 @@
 import dotenv from 'dotenv'
 import express from 'express'
 
-const result = dotenv.config({ path: '../.env'})
+const result = dotenv.config({path: `/../.env`})
 const app = express()
 
 
-if (result.error) {
-    throw result.error
-}
+// if (result.error) {
+//     throw result.error
+// }
+// console.log(result.parsed)
 
-console.log(result.parsed)
 const port = process.env.PORT || 5000
 
 app.listen(port, () => console.log(`Server running on ${port}`))
